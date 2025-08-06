c95af02de2c7b4324765d7fdf2a1c11f01a7d6fc
SolovisionDesignsLLC <mattsolo799@gmail.com>
2025-08-06 17:36:25 -0400

    Initial Commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by Solovision Designs on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myFirstAppTests/myFirstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppTests.swift
+//  myFirstAppTests
+//
+//  Created by Solovision Designs on 8/6/25.
+//
+
+import Testing
+@testable import myFirstApp
+
+struct myFirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
